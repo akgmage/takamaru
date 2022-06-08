300bedf42987c4ab1ed5d3e30c61ce65b8d83dba
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-08 09:58:03 +0530

    Add comment

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	httpMethod := "GET"
 	url := "https://api.github.com"
 
-	client := http.Client{}
+	client := http.Client{} // use this client to perform any http method you need
 
 	request, err := http.NewRequest(httpMethod, url, nil)
 	request.Header.Set("Accept", "application/json")
@@ -22,7 +22,7 @@ func main() {
 	}
 
 	defer response.Body.Close()
-	bytes, err := ioutil.ReadAll(response.Body)
+	bytes, err := ioutil.ReadAll(response.Body) // Read from response until an error or EOF and return the data it read
 	if err != nil {
 		panic(err)
 	}
